31960ca810d7bd65f8021ef5183e2c5ddd2251c9
SimBa296 <yuudai07ya@gmail.com>
2023-07-15 23:11:54 +0900

    ♻️ headerの改善

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { Footer } from "@/Components/Footer";
 import { Main } from "@/Components/Main";
+import { Header } from "@/Components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
     <main>
       <Main page="about" />
       <Footer />
+      <Header />
     </main>
   );
 }
