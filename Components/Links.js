07d29d2fb3c16d445bb1b498bf763f5a59770628
SimBa296 <yuudai07ya@gmail.com>
2023-07-22 21:52:26 +0900

    🐛

--- Components/Links.js
+++ Components/Links.js
@@ -1,12 +1,60 @@
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description: "Find in-depth information about Next.js features and API.",
+    title: "Docs",
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description:
+      "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+    title: "Learn",
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description:
+      "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+    title: "Templates",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description:
+      "Instantly deploy your Next.js site to a shareable URL with Vercel.",
+    title: "Deploy",
+  },
+];
 
-export  function Links() {
+export function Links() {
   return (
-    <main>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
+    <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {ITEMS.map((item,index) => {
+        return (
+          <a
+          key={index}
+            href={item.href}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              {item.title}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              {item.description}
+            </p>
+          </a>
+        )
+        })}
+
+      {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -72,8 +120,7 @@ export  function Links() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
-      </div>
-    </main>
+        </a> */}
+    </div>
   );
 }
