5d99da1f8ea39f8b4d29d38877c29af5290abdb6
SimBa296 <yuudai07ya@gmail.com>
2023-07-25 21:29:46 +0900

    🎨

--- Components/Links.js
+++ Components/Links.js
@@ -30,31 +30,31 @@ const ITEMS = [
 export function Links() {
   return (
     <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-      {ITEMS.map((item,index) => {
+      {ITEMS.map((item, index) => {
         return (
           <a
-          key={index}
+            key={index}
             href={item.href}
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              {item.title}
+              ◆{item.title}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            
+
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
               {item.description}
             </p>
           </a>
-        )
-        })}
+        );
+      })}
 
-      {/* <a
+      {/* {[
+        <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -69,7 +69,7 @@ export function Links() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
           </p>
-        </a>
+        </a>,
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -86,7 +86,7 @@ export function Links() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-        </a>
+        </a>,
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -103,7 +103,7 @@ export function Links() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Discover and deploy boilerplate example Next.js&nbsp;projects.
           </p>
-        </a>
+        </a>,
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -120,7 +120,8 @@ export function Links() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a> */}
+        </a>,
+      ]} */}
     </div>
   );
 }
