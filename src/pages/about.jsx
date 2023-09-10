2c80e4d4aad825657810d1075b500d2f0fd76e77
SimBa296 <yuudai07ya@gmail.com>
2023-09-11 08:24:49 +0900

    🏗️ 拡張子・ディレクトリ構成・インポートの変更

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
-import { Footer } from "../Components/Footer";
-import { Main } from "../Components/Main";
-import { Header } from "../Components/Header";
+import { Footer } from "src/Components/Footer";
+import { Main } from "src/Components/Main";
+import { Header } from "src/Components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
