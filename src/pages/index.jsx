c2a9cc8d5879788cf66e8391150fe26d175464a2
Lab5 <yuudai07ya@gmail.com>
2023-10-09 19:13:56 +0900

    ⚡️ クリックイベントと関数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,33 @@ import { Inter } from "next/font/google";
 import { Footer } from "src/Components/Footer";
 import { Main } from "src/Components/Main";
 import { Header } from "src/Components/Header";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const handleClick = (e) => {
+  console.log(e.target.href);
+  e.preventDefault();
+  alert(foo);
+};
+
 export default function Home() {
+  const foo = 1;
+
+  // const handleClick = (e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  //   alert(foo);
+  // };
+
   return (
     <main>
       <Header />
+      <Link
+        href="/about"
+        onClick={handleClick}
+      >ボタン</Link>
+
       <Main page="index" />
       <Footer />
     </main>
